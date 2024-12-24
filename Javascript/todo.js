821df380c3fc4b26732fc17c97c0b528658a9a24
Jungman Bae <jmbae@live.com>
2024-12-24 14:25:46 +0900

    자바스크립트 실행 시점 확인

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -1,1 +1,7 @@
-console.log("todo.js OK!")
+// 2. 웹 페이지가 로드되면 실행됨
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded');
+});
+
+// 1. 파일이 실행되면 실행됨
+console.log('todo.js');
