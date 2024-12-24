3837636aed47a61ae0817694e73fc6105e2f0eb0
Jungman Bae <jmbae@live.com>
2024-12-24 14:57:42 +0900

    주석 추가

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -2,9 +2,9 @@ function addTodo(text) {
   // 문자열 보간법
   console.log(`addTodo: ${text}`);
   const todoList = document.getElementById('todoList');
-  const li = document.createElement('li');
-  li.textContent = text;
-  todoList.appendChild(li);
+  const li = document.createElement('li'); // <li></li> 생성
+  li.textContent = text; // <li>text</li> 내용 추가
+  todoList.appendChild(li); // <ul><li>text</li></ul> 추가
 }
 
 document.getElementById('addTodo').addEventListener('click', () => {
