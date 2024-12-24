cce006f4f040e39066cbf89fdb4dc982ecba49cf
Jungman Bae <jmbae@live.com>
2024-12-24 14:46:30 +0900

    addTodo 함수 비즈니스 로직 추가

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -1,10 +1,14 @@
 function addTodo(text) {
   // 문자열 보간법
   console.log(`addTodo: ${text}`);
-
+  const todoList = document.getElementById('todoList');
+  const li = document.createElement('li');
+  li.textContent = text;
+  todoList.appendChild(li);
 }
 
 document.getElementById('addTodo').addEventListener('click', () => {
   const input = document.getElementById('todoInput');
   addTodo(input.value);
+  input.value = '';
 });
