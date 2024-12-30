7b425da0ab30a4189e3a3121ef9d303b041996fe
JM Bae <jmbae@live.com>
2024-12-30 09:40:06 +0900

    Playground 실습 1: 상수

---
+++ 02-Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+let phrase: String = "The quick brown fox jumps over the lazy dog"
+let numberOfFoxes: Int = (1+1) // 2
+let numberOfAnimals: Int = 2
+
+let avarageCharactersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
+print(avarageCharactersPerWord)
+
+// 상수는 수정 불가능
+// phrase = "The quick brown fox jumps over the lazy dog"
