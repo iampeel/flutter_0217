3859a39dd28733a7ba1af10682cba87f4401a21e
JM Bae <jmbae@live.com>
2024-12-31 13:21:32 +0900

    클래스 상속: Family

--- 02-Playgrounds/Basics.playground/Contents.swift
+++ 02-Playgrounds/Basics.playground/Contents.swift
@@ -22,7 +22,7 @@ class Person {
     func fullName() -> String {
         var fullName: String = ""
         if let middleName { fullName += middleName + " " }
-        fullName += givenName + " " + familyName
+        fullName += self.givenName + " " + self.familyName
         return fullName
     }
     
@@ -30,6 +30,29 @@ class Person {
         return Person(givenName: "John", familyName: "Doe")
     }
 }
+
+final class Friend: Person {
+    var whereWeMet: String?
+    
+    override var displayName: String {
+        return super.displayName + " - Where we met: \(self.whereWeMet ?? "Unknown")"
+    }
+}
+//class BusinessRelation: Friend {} // final 로 인해 더이상 상속 불가
+
+final class Family: Person {
+    let relationship: String
+    
+    init(givenName: String, middleName: String? = nil, familyName: String = "Barker", relationship: String) {
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
+    
+    override var displayName: String {
+        return super.displayName + " - Relationship: \(self.relationship)"
+    }
+}
+
 
 let person = Person.createDummyPerson()
 print(person.fullName())
@@ -42,3 +65,20 @@ person3.country = "US"
 print(person3.displayName)
 print(person.displayName)
 
+let friend = Friend(givenName: "길동", familyName: "홍")
+friend.whereWeMet = "London"
+print(friend.displayName)
+
+print("------------------------------")
+
+let steve = Person(givenName: "Steve", middleName: "Paul", familyName: "Jobs")
+let sam = Friend(givenName: "Sam", middleName: "Now", familyName: "Rowley")
+sam.whereWeMet = "Work together at Jaguar Land Rover"
+let maddie = Family(givenName: "Maddie", middleName: "Rose", relationship: "Daughter")
+let mark = Family(givenName: "Mark", middleName: "David", familyName: "Pendlebury", relationship: "Brother-In-Law")
+mark.country = "USA"
+
+print(steve.displayName)
+print(sam.displayName)
+print(maddie.displayName)
+print(mark.displayName)
