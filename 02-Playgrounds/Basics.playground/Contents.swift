d197cc04e9de29926a273c1793f164ce6c8178b4
JM Bae <jmbae@live.com>
2024-12-31 11:22:59 +0900

    객체 실습: Person

--- 02-Playgrounds/Basics.playground/Contents.swift
+++ 02-Playgrounds/Basics.playground/Contents.swift
@@ -1,5 +1,44 @@
 import Foundation
 
 //runOptional()
+//runFunction()
 
-runFunction()
+class Person {
+    let givenName: String
+    let middleName: String?
+    let familyName: String
+    var country: String = "KR"
+    
+    init(givenName: String, middleName: String? = nil, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    var displayName: String {
+        return "\(fullName()) - Location: \(country)"
+    }
+    
+    func fullName() -> String {
+        var fullName: String = ""
+        if let middleName { fullName += middleName + " " }
+        fullName += givenName + " " + familyName
+        return fullName
+    }
+    
+    static func createDummyPerson() -> Person {
+        return Person(givenName: "John", familyName: "Doe")
+    }
+}
+
+let person = Person.createDummyPerson()
+print(person.fullName())
+
+let person2 = Person(givenName: "Jane", familyName: "Doe")
+print(person2.fullName())
+
+let person3 = Person.createDummyPerson()
+person3.country = "US"
+print(person3.displayName)
+print(person.displayName)
+
