b0722f9ce4101a134ee846153feb5be595d25518
JM Bae <jmbae@live.com>
2025-01-02 17:01:49 +0900

    try, do, catch 2

--- 02-Playgrounds/Basics.playground/Contents.swift
+++ 02-Playgrounds/Basics.playground/Contents.swift
@@ -122,12 +122,15 @@ enum MealState {
 }
 
 enum MealError: Error {
-    case canOnlyMoveToAppropriateState
+    case canOnlyMoveToAppropriateState(from: MealState, to: MealState)
+    case tooMuchSalt
+    case wrongStateToAddSalt
 }
 
 class Meal {
     private(set) var state: MealState = .initial
-    
+    private(set) var saltAdded = 0
+
     func change(to newState: MealState) throws {
         switch (state, newState) {
         case (.initial, .buyIngredients),
@@ -137,7 +140,18 @@ class Meal {
             (.plateUp, .serve):
             state = newState
         default:
-            throw MealError.canOnlyMoveToAppropriateState
+            throw MealError.canOnlyMoveToAppropriateState(from: state, to: newState)
+        }
+    }
+    
+    func addSalt() throws {
+        print("addSalt >> \(saltAdded) / \(state)")
+        if saltAdded >= 5 {
+            throw MealError.tooMuchSalt
+        } else if .initial == state || .buyIngredients == state {
+            throw MealError.wrongStateToAddSalt
+        } else {
+            saltAdded += 1
         }
     }
     
@@ -167,14 +181,19 @@ do {
 //    try dinner.change(to: .plateUp)
 //    try dinner.change(to: .serve)
     
-    try dinner.buyIngredients()
+    try dinner.addSalt()
     try dinner.prepareIngredients()
+    try dinner.buyIngredients()
     try dinner.cook()
     try dinner.plateUp()
     try dinner.serve()
     
     print("저녁 식사가 준비되었습니다!")
-} catch let error {
-    print(error)
+} catch MealError.canOnlyMoveToAppropriateState(let from, let to) {
+    print("\(from) -> \(to) 상태로 이동할 수 없습니다.")
+} catch MealError.tooMuchSalt {
+    print("짜다!")
+} catch MealError.wrongStateToAddSalt {
+    print("이 단계에서는 소금을 추가할 수 없습니다.")
 }
 
