77839be2d707d3a7d66596d8bdec25c7f64748ec
JM Bae <jmbae@live.com>
2025-01-02 13:41:22 +0900

    조건문

--- 02-Playgrounds/Basics.playground/Contents.swift
+++ 02-Playgrounds/Basics.playground/Contents.swift
@@ -15,16 +15,21 @@ enum PoolBallType {
     case black
 }
 
-func poolBallType(forNumber number: Int) -> PoolBallType {
-    if number < 8 {
+func poolBallType(forNumber number: Int) -> PoolBallType? {
+    if number < 8 && number > 0 {
         return .solid
-    } else if number > 8 {
+    } else if number > 8 && number < 16 {
         return .stripe
-    } else {
+    } else if number == 8 {
         return .black
+    } else {
+        return nil
     }
 }
 
 let two = poolBallType(forNumber: 2)
 let eight = poolBallType(forNumber: 8)
 let twelve = poolBallType(forNumber: 12)
+
+let zero = poolBallType(forNumber: 0)
+let sixteen = poolBallType(forNumber: 16)
