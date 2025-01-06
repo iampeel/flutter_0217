499b530ecf19b56631090855d6aaf2bf13c65419
JM Bae <jmbae@live.com>
2025-01-06 15:12:53 +0900

    JSON 요청과 비동기 함수의 콜백(completionHandler)

--- 02-Playgrounds/GithubRepo.playground/Contents.swift
+++ 02-Playgrounds/GithubRepo.playground/Contents.swift
@@ -2,23 +2,42 @@ import Foundation
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-func fetchRepos(forUsername username: String) async throws {
+enum ResponsError: Error {
+    case requestFailed
+    case unexpectedResponseStructure
+}
+
+func fetchRepos(forUsername username: String,
+                completionHandler: @escaping ([[String: Any]]?, ResponsError?) -> Void) async {
     let urlString = "https://api.github.com/users/\(username)/repos"
     let url = URL(string: urlString)!
     
     var request = URLRequest(url: url)
     request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
-    request.setValue("SwiftPlayground", forHTTPHeaderField: "User-Agent")
+//    request.setValue("SwiftPlayground", forHTTPHeaderField: "User-Agent")
     
     do {
+        // 비동기 실행 이전
         let (data, _) = try await URLSession.shared.data(for: request)
+        // 비동기 실행 이후
         let deserialized = try JSONSerialization.jsonObject(with: data, options: [])
-        print(deserialized)
+        guard let repos = deserialized as? [[String: Any]] else {
+            completionHandler(nil, .unexpectedResponseStructure)
+            return
+        }
+        completionHandler(repos, nil)
     } catch {
         print(error)
+        completionHandler(nil, .requestFailed)
     }
 }
 
 Task {
-    try await fetchRepos(forUsername: "apple")
+    await fetchRepos(forUsername: "apple") { repos, error in
+        if let error {
+            print("Error: \(error)")
+            return
+        }
+        print("Repos: \(repos!.count)")
+    }
 }
