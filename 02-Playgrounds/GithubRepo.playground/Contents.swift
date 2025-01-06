f3b5297ab3d2c3867343c204aa4ac109bf950a9e
JM Bae <jmbae@live.com>
2025-01-06 15:37:06 +0900

    어색한 async 구문 삭제

--- 02-Playgrounds/GithubRepo.playground/Contents.swift
+++ 02-Playgrounds/GithubRepo.playground/Contents.swift
@@ -8,7 +8,7 @@ enum ResponsError: Error {
 }
 
 func fetchRepos(forUsername username: String,
-                completionHandler: @escaping ([[String: Any]]?, ResponsError?) -> Void) async {
+                completionHandler: @escaping ([[String: Any]]?, ResponsError?) -> Void) {
     let urlString = "https://api.github.com/users/\(username)/repos"
     let url = URL(string: urlString)!
     
@@ -16,28 +16,30 @@ func fetchRepos(forUsername username: String,
     request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
 //    request.setValue("SwiftPlayground", forHTTPHeaderField: "User-Agent")
     
-    do {
-        // 비동기 실행 이전
-        let (data, _) = try await URLSession.shared.data(for: request)
-        // 비동기 실행 이후
-        let deserialized = try JSONSerialization.jsonObject(with: data, options: [])
-        guard let repos = deserialized as? [[String: Any]] else {
-            completionHandler(nil, .unexpectedResponseStructure)
-            return
+    Task {
+        do {
+            // 비동기 실행 이전
+            let (data, response) = try await URLSession.shared.data(for: request)
+            print(response)
+            // 비동기 실행 이후
+            let deserialized = try JSONSerialization.jsonObject(with: data, options: [])
+            guard let repos = deserialized as? [[String: Any]] else {
+                completionHandler(nil, .unexpectedResponseStructure)
+                return
+            }
+            completionHandler(repos, nil)
+        } catch {
+            print(error)
+            completionHandler(nil, .requestFailed)
         }
-        completionHandler(repos, nil)
-    } catch {
-        print(error)
-        completionHandler(nil, .requestFailed)
     }
 }
 
-Task {
-    await fetchRepos(forUsername: "apple") { repos, error in
-        if let error {
-            print("Error: \(error)")
-            return
-        }
-        print("Repos: \(repos!.count)")
+fetchRepos(forUsername: "apple") { repos, error in
+    if let error {
+        print("Error: \(error)")
+        return
     }
+    print("Repos: \(repos!.count)")
 }
+
