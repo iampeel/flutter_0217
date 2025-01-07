524232c12be7d8aca99fb58b188ca2ab0f0d7df0
JM Bae <jmbae@live.com>
2025-01-07 11:45:32 +0900

    타입 캐스팅

--- 04-SwiftUI/OptionalTypes.playground/Contents.swift
+++ 04-SwiftUI/OptionalTypes.playground/Contents.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 var index: Int?
 index = 3
 
@@ -8,6 +10,8 @@ if index != nil {
 } else {
     print("값이 할당되지 않았을때")
 }
+
+// 조건부 언래핑
 // if let index = index => 축약해서 사용 가능
 if let index {
     // 이 중괄호 안에서는 옵셔널이 아닌 Int 타입으로 사용 가능
@@ -29,3 +33,34 @@ if let name1 = optionalName1,
 }
 
 
+var myDictionary = Dictionary<String, Int>()
+myDictionary["Swift"] = 100
+myDictionary["Programming"] = 200
+myDictionary["Language"] = 300
+
+// 딕셔너리의 값 엑세스는 값이 없을 수 있기 때문에, 옵셔널타입으로 값을 전달
+print(myDictionary["C++"] ?? 0) // 기본값
+if let value = myDictionary["C++"] {
+    print(value)
+} else {
+    print(0)
+}
+
+// 타입 캐스팅
+let value = myDictionary["Swift"] as! Int
+print(value)
+
+// UIView -> UIControl -> UIButton 순으로 상속 관계
+let myButton: UIButton = UIButton()
+let myControl = myButton as UIControl // 타입 캐스팅 ( 업 캐스팅 )
+
+// UIScrollView ->  UITextView
+let myScrollView: UIScrollView = UIScrollView()
+//let myTextView = myScrollView as! UITextView // 강제 타입 캐스팅 ( 다운 캐스팅 )
+
+// 조건부 타입 캐스팅
+if let myTextView = myScrollView as? UITextView {
+    print(myTextView.text ?? "")
+} else {
+    print("UITextView 로 타입 캐스팅 하지 못했습니다.")
+}
