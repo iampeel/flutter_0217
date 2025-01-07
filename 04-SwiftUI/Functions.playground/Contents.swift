f50c22fec7ccaa011631ec2413e884db646249dc
JM Bae <jmbae@live.com>
2025-01-07 15:08:30 +0900

    함수2: 가변 매개변수, inout, 함수 타입 변수

--- 04-SwiftUI/Functions.playground/Contents.swift
+++ 04-SwiftUI/Functions.playground/Contents.swift
@@ -1,5 +1,5 @@
 
-// 함수 선언 부분
+// 함수 정의(Definition) 부분
 func sayHello() /* -> Void 생략 */ {
     print("Hello")
 //    return 이 없음. Void
@@ -30,3 +30,43 @@ let message4 = buildMessageFor("아무개", count: 100)
 print(message3)
 print(message4)
 
+func displayStrings(_ strings: String...) {
+    for string in strings { // strings [String]
+        print(string)
+    }
+}
+
+displayStrings("Hello", "World", "Swift", "Programming")
+
+func displayStrings(_ strings: String..., counts: Int...) {
+    for string in strings { // strings [String]
+        print(string)
+    }
+    print("count: \(counts)")
+}
+
+displayStrings("Hello", "World", "Swift", "Programming", counts: 1, 2, 3)
+
+// inout 키워드: 파라미터 값을 외부에도 적용해주는 키워드
+func doubleValue(_ value: inout Int) /* -> Void */ {
+    value *= 2 // value = value * 2
+    print(value)
+}
+
+var myValue: Int = 10
+doubleValue(&myValue)
+print(myValue)
+
+
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+let mathFunction: (Int, Int) -> Int = add
+print(mathFunction(2, 3)) // 5
+
+let multiply = { (a: Int, b: Int) -> Int in
+    return a * b
+}
+
+print(multiply(2, 3)) 
