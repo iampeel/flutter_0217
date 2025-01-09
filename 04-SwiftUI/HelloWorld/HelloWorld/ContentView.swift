2f8484d37977206ae0a341267a9dcfac25a40170
JM Bae <jmbae@live.com>
2025-01-09 14:41:43 +0900

    동적 레이아웃, 텍스트 줄바뀜 제한, 우선순위

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -8,53 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var myLayout: AnyLayout = AnyLayout(VStackLayout())
+    
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
+        myLayout {
+            Text("Sample Text")
+            Text("Sample Text")
+            Text("Sample Text")
+        }
+        HStack {
+            Button(action: {
+                myLayout = AnyLayout(HStackLayout())
+            }) {
+                Text("HStack")
             }
-            ZStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
-            }
-            // 복잡한 중첩 레이아웃 예시
-            Text("Financial Results")
-                .font(.title)
-            HStack {
-                Text("Q1 Sales")
-                    .font(.headline)
-                VStack {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                VStack {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-            }
-            // 정렬, 여백
-            HStack(alignment: .top) {
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
+            Button(action: {
+                myLayout = AnyLayout(VStackLayout())
+            }) {
+                Text("VStack")
             }
         }
+        
+        HStack {
+            Image(systemName: "airplane")
+            Text("Flight times:")
+                .layoutPriority(1) // 우선순위 부여
+            Text("London")
+        }
+        .font(.largeTitle)
+        .lineLimit(1) // 글씨 크기로 인해 줄 바뀜을 제한
     }
 }
 
