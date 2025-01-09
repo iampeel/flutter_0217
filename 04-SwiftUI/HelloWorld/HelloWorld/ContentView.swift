0caaec0baec668d60a038055bed8492383b9a7e3
JM Bae <jmbae@live.com>
2025-01-09 11:46:51 +0900

    버튼의 다른 생성자 함수 사용, foregroundStyle 수정

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -19,7 +19,8 @@ struct ContentView: View {
             Text("\(count)")
                 .font(.system(size: 100))
                 .fontWeight(.bold)
-                .foregroundColor(count % 2 == 0 ? .blue : .red) // 짝수/홀수에 따른 색상 변경
+                // 앞으로 foregroundColor는 foregroundStyle로 변경해서 사용합니다.
+                .foregroundStyle(count % 2 == 0 ? .blue : .red) // 짝수/홀수에 따른 색상 변경
             
             HStack { // 수평 스택으로 버튼 정렬
                 Button(action: {
@@ -29,12 +30,20 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .padding()
                         .background(Color.gray)
-                        .foregroundColor(.white)
+                        .foregroundStyle(.white)
                         .clipShape(Circle())
                 })
                 
                 Spacer() // 중간 여백
                 
+                // 버튼의 다른 생성자 함수 사용
+                Button("+2", action:{ // action 클로저
+                    self.count += 2
+                })
+                .font(.largeTitle)
+
+                Spacer() // 중간 여백
+
                 Button(action: {
                     self.count += 1
                 }) {
@@ -42,7 +51,7 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .padding()
                         .background(Color.green)
-                        .foregroundColor(.white)
+                        .foregroundStyle(.white)
                         .clipShape(Circle())
                 }
             }
