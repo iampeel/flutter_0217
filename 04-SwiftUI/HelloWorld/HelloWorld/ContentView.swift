a03bce2afd173309a1055234e16f82d4b54c3d70
JM Bae <jmbae@live.com>
2025-01-09 13:44:11 +0900

    레이블, 레이블 스타일

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -22,7 +22,18 @@ struct MyVStack<Content: View>: View {
     }
 }
 
-struct ContentView: View {    
+// 커스텀 레이블 스타일 (레이아웃)
+struct VerticalLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .center) {
+            configuration.icon
+                .padding(5)
+            configuration.title
+        }
+    }
+}
+
+struct ContentView: View {
     var body: some View {
         MyVStack {
             Text("Text 1")
@@ -32,6 +43,25 @@ struct ContentView: View {
                 Image(systemName: "star.fill")
                 Image(systemName: "star")
             }
+            Button(action:{
+                print("click")
+            })
+            {
+                Label("Welcome to SwiftUI",
+                      systemImage: "person.circle.fill")
+            }
+            Label(title: {
+                Text("Welcome to SwiftUI")
+                    .font(.largeTitle)
+            },
+                  icon: {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 25, height: 25)
+            })
+            .labelStyle(.titleAndIcon)
+            Label("수직 레이블", systemImage: "moon.fill")
+                .labelStyle(VerticalLabelStyle())
         }
     }
 }
