597beda39e0dde86024b92fa2e16453ba7cc0074
JM Bae <jmbae@live.com>
2025-01-09 15:13:56 +0900

    프레임, 크기, 안전영역 무시

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Spacer()
+        Text("Hello World")
+            .font(.largeTitle)
+//            .frame(minWidth: 100, maxWidth: 300, minHeight: 100, maxHeight: 100, alignment: .center)
+            .border(Color.black)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.red)
+//            .ignoresSafeArea(.all)
+//            .frame(width: 300, height: 100, alignment: .center)
     }
 }
 
