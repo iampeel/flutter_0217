8e5e128bc7308b82ed3a58530e2c463c620bb064
JM Bae <jmbae@live.com>
2025-01-09 14:25:12 +0900

    레이아웃, 정렬, 여백 등

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                Image(systemName: "goforward.10")
+                Image(systemName: "goforward.15")
+                Image(systemName: "goforward.30")
+            }
+            ZStack {
+                Image(systemName: "goforward.10")
+                Image(systemName: "goforward.15")
+                Image(systemName: "goforward.30")
+            }
+            // 복잡한 중첩 레이아웃 예시
+            Text("Financial Results")
+                .font(.title)
+            HStack {
+                Text("Q1 Sales")
+                    .font(.headline)
+                VStack {
+                    Text("January")
+                    Text("February")
+                    Text("March")
+                }
+                VStack {
+                    Text("$1000")
+                    Text("$200")
+                    Text("$3000")
+                }
+            }
+            // 정렬, 여백
+            HStack(alignment: .top) {
+                Text("Q1 Sales")
+                    .font(.headline)
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("January")
+                    Text("February")
+                    Text("March")
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("$1000")
+                    Text("$200")
+                    Text("$3000")
+                }
+            }
+        }
     }
 }
 
