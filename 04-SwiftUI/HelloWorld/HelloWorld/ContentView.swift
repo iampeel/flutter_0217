d394023383c4df1606a57015fc486281fe85e3b5
JM Bae <jmbae@live.com>
2025-01-09 14:10:35 +0900

    Clear

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -7,62 +7,9 @@
 
 import SwiftUI
 
-struct MyVStack<Content: View>: View {
-    let content: () -> Content
-    
-    init(@ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-    }
-    
-    var body: some View {
-        VStack(spacing: 10) {
-            content()
-        }
-        .font(.largeTitle)
-    }
-}
-
-// 커스텀 레이블 스타일 (레이아웃)
-struct VerticalLabelStyle: LabelStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        VStack(alignment: .center) {
-            configuration.icon
-                .padding(5)
-            configuration.title
-        }
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        MyVStack {
-            Text("Text 1")
-            Text("Text 2")
-            HStack {
-                Image(systemName: "star.fill")
-                Image(systemName: "star.fill")
-                Image(systemName: "star")
-            }
-            Button(action:{
-                print("click")
-            })
-            {
-                Label("Welcome to SwiftUI",
-                      systemImage: "person.circle.fill")
-            }
-            Label(title: {
-                Text("Welcome to SwiftUI")
-                    .font(.largeTitle)
-            },
-                  icon: {
-                Circle()
-                    .fill(Color.blue)
-                    .frame(width: 25, height: 25)
-            })
-            .labelStyle(.titleAndIcon)
-            Label("수직 레이블", systemImage: "moon.fill")
-                .labelStyle(VerticalLabelStyle())
-        }
+        Text("Hello, World!")
     }
 }
 
