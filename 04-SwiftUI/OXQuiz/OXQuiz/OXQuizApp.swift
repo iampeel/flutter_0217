6b39b2e9a6e3aebd7b53f93c30aef5106199b094
JM Bae <jmbae@live.com>
2025-01-10 17:06:47 +0900

    OX퀴즈 프로젝트

---
+++ 04-SwiftUI/OXQuiz/OXQuiz/OXQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXQuizApp.swift
+//  OXQuiz
+//
+//  Created by Jungman Bae on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OXQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
