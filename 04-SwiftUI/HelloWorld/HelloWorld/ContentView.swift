2c8f02f665bcb8b6994199e81e5ad5f1d85a1e31
JM Bae <jmbae@live.com>
2025-01-10 09:31:53 +0900

    탭 제스처

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -8,43 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var colors: [Color] = [.black, .red, .green, .blue]
-    var colorNames = ["Black", "Red", "Green", "Blue"]
-    @State private var colorIndex = 0
-    @State private var rotation: Double = 0
-    @State private var text: String = "Welcome to SwiftUI"
+    @State private var isTapped = false
     
     var body: some View {
-        VStack {
-            List {}
-
-            Text(text)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .rotationEffect(.degrees(rotation))
-                .animation(.easeInOut(duration: 5), value: rotation)
-                .foregroundColor(colors[colorIndex])
-            
-            Slider(value: $rotation, in: 0...360, step: 0.1)
-            
-            TextField("Enter text here", text: $text)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            List {
-                // 기본 피커 스타일을 ListItem 으로 사용할 경우 Label 이 표시됨
-                // 기본 스타일 에서는 레이블이 표시되지 않음.
-                // ( 접근성을 통한 VoiceOver 음성 안내에서만 활용됨 )
-                Picker("Color", selection: $colorIndex) {
-                    ForEach(0..<colorNames.count, id: \.self) { color in
-                        Text(colorNames[color])
-                            .foregroundColor(colors[color])
-                    }
-                }
+        Text("Tap me!")
+            .font(.largeTitle)
+            .padding()
+            .background(isTapped ? Color.green : Color.red)
+            .animation(.default, value: isTapped)
+            // Tap 카운트를 이용해 실행시킬 탭 횟수를 조절할 수 있음
+            .onTapGesture(count: 3) {
+                isTapped.toggle()
             }
-            
-        }
-    }
-}
+    }}
 
 #Preview {
     ContentView()
