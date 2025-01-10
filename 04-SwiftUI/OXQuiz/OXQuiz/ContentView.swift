56aba257917fcdaa10fe8eb108318f1263bd6e6e
JM Bae <jmbae@live.com>
2025-01-10 17:21:03 +0900

    OX퀴즈: UI 구성

--- 04-SwiftUI/OXQuiz/OXQuiz/ContentView.swift
+++ 04-SwiftUI/OXQuiz/OXQuiz/ContentView.swift
@@ -10,12 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("다음 수식은 맞을까요?")
+                .font(.largeTitle)
+                .padding(.top, 20)
+            Spacer()
+            Text("8 x 0 = 5")
+                .font(.largeTitle)
+                .padding()
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {},
+                       label: {
+                    Label("맞음", systemImage: "checkmark.diamond.fill")
+                        .foregroundStyle(.green)
+                    })
+                Spacer()
+                Button(action: {},
+                       label: {
+                    Label("틀림", systemImage: "xmark.diamond")
+                        .foregroundStyle(.red)
+                })
+                Spacer()
+            }
+            .font(.system(size: 35, weight: .bold))
+            Spacer()
+            HStack {
+                Spacer()
+                Text("0개 맞춤")
+                Spacer()
+                Text("0개 틀림")
+                Spacer()
+            }
+            .font(.system(size: 30))
+            Spacer()
+            Button("카운트 초기화", action: {})
+                .font(.system(size: 30))
+                .padding(.bottom, 20)
         }
-        .padding()
     }
 }
 
