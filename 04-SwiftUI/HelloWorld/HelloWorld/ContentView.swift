04fce6a14ff766993437d2d76177252428a3e6d3
JM Bae <jmbae@live.com>
2025-01-10 11:37:08 +0900

    챕터25: 액터

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -7,41 +7,9 @@
 
 import SwiftUI
 
-enum DurationError: Error {
-    case tooLong
-    case tooShort
-}
-
 struct ContentView: View {
     var body: some View {
-        Button(action: {
-            Task {
-                await doSomething()
-            }
-        }) {
-            Text("Do Something")
-        }
-    }
-    
-    func doSomething() async {
-        var timeStamps: [Int: Date] = [:]
-        timeStamps = await withTaskGroup(of: (Int, Date).self) { group in
-            for i in 1...5 {
-                group.addTask {
-                    return (i, await takesTooLong())
-                }
-            }
-            var results: [Int: Date] = [:]
-            for await (task, date) in group {
-                results[task] = date
-            }
-            return results
-        }
-    }
-    
-    func takesTooLong() async -> Date {
-        sleep(5)
-        return Date()
+        Text("Hello World!")
     }
 }
 
