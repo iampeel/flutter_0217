fa1df49b61f7531d7b8fd5d88e06a4d72535c21a
JM Bae <jmbae@live.com>
2025-01-10 10:07:10 +0900

    챕터24 스위프트 구조화된 동시성 개요

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -8,20 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert = false
-    
     var body: some View {
-        Button("Show Alert") {
-            showAlert = true
+        Button(action: {
+            doSomething()
+        }) {
+            Text("Do Something")
         }
-        // 경고창 팝업을 띄우는 수정자
-        .alert("Important Message",
-               isPresented: $showAlert,
-               actions: {
-            Button("OK", role: .cancel) { }
-        }, message: {
-            Text("This is an alert message.")
-        })
+    }
+    
+    func doSomething() {
+    }
+    
+    func takesTooLong() {
     }
 }
 
