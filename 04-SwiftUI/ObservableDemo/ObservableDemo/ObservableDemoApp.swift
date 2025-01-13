bc077cd14aec9bebd41a249c184c0a6d7873e9ef
JM Bae <jmbae@live.com>
2025-01-13 11:28:54 +0900

    챕터27: Observable

---
+++ 04-SwiftUI/ObservableDemo/ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by Jungman Bae on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
