8809823e7e832642608677b1fba678ad06f1c707
JM Bae <jmbae@live.com>
2025-01-13 09:38:07 +0900

    onAppear,onDisappear,onChange

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World!")
+        TabView {
+            FirstTabView()
+                .tabItem {
+                    Image(systemName: "01.circle")
+                    Text("First")
+                }
+            SecondTabView()
+                .tabItem {
+                    Image(systemName: "02.circle")
+                    Text("Second")
+                }
+        }
     }
 }
 
---
+++ 04-SwiftUI/HelloWorld/HelloWorld/SecondTabView.swift
@@ -0,0 +1,25 @@
+//
+//  SecondTabView.swift
+//  HelloWorld
+//
+//  Created by Jungman Bae on 1/13/25.
+//
+
+import SwiftUI
+
+struct SecondTabView: View {
+    @State private var text: String = ""
+
+    var body: some View {
+        TextEditor(text: $text)
+            .padding()
+            // text 변수의 변경시 실행되는 수정자
+            .onChange(of: text, perform: { value in
+                print("onChange triggered")
+            })
+    }
+}
+
+#Preview {
+    SecondTabView()
+}
