07323e6ad25cfad6a19a3c80eefbfc7b6dcf1d64
JM Bae <jmbae@live.com>
2025-01-13 13:23:34 +0900

    unowned self

--- 04-SwiftUI/ObservableDemo/ObservableDemo/TimerData.swift
+++ 04-SwiftUI/ObservableDemo/ObservableDemo/TimerData.swift
@@ -12,7 +12,7 @@ class TimerData: ObservableObject {
     @Published var timeCount = 0
     var timer: Timer?
     
-    init() {        
+    init() {
 //      기존 셀렉터 호출 방식 : #selector 필요
 //        timer = Timer.scheduledTimer(timeInterval: 1.0,
 //                                     target: self,
@@ -21,9 +21,14 @@ class TimerData: ObservableObject {
 //                                     repeats: true)
         
         // [weak self] 는 변수 캡쳐 시 ARC 카운트가 증가하지 않도록 해서, 메모리 누수를 막는다.
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
-            self?.timerDidFire()
+        // [unowned self] 는 강제 언래핑된 self 키워드, 조심해서 사용해야함
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [unowned self] _ in
+            self.timerDidFire()
         }
+    }
+    
+    deinit {
+        timer?.invalidate()
     }
     
     /*@objc 기존 셀렉터 방식의 호출: 반드시 함수 정의시에 @objc 키워드 사용 */ func timerDidFire() {
