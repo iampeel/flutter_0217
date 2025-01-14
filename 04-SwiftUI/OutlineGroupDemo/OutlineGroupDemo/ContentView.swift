bd5f31e62b89aa00328400852a9e3fa59db305bc
JM Bae <jmbae@live.com>
2025-01-14 15:26:24 +0900

    챕터 35: OutlineGroupDemo

---
+++ 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OutlineGroupDemo
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/OutlineGroupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlineGroupDemoApp.swift
+//  OutlineGroupDemo
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct OutlineGroupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
