febc60b7853621587272ca00e6512e14b3f7489a
JM Bae <jmbae@live.com>
2025-01-14 15:33:32 +0900

    리스트 뷰 추가

--- 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/ContentView.swift
+++ 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(carItems, children: \.children) { item in
+            CellView(item: item)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CellView: View {
+    var item: CarInfo
+    
+    var body: some View {
+        HStack {
+            Image(systemName: item.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 25, height: 25)
+                .foregroundStyle(.green)
+            Text(item.name)
+        }
+    }
 }
