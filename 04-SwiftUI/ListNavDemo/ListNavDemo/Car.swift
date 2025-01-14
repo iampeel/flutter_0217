2b3ae81e7e0cdc007c6ff87c797f3011cc1ad5c5
JM Bae <jmbae@live.com>
2025-01-14 14:07:37 +0900

    Car 구조체 추가

---
+++ 04-SwiftUI/ListNavDemo/ListNavDemo/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  ListNavDemo
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import Foundation
+
+struct Car: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+    var imageName: String
+    var isHybrid: Bool
+}
