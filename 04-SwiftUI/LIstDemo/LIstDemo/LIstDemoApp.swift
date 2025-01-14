09429e01c96dd5344cfe6c12c5360673ba009209
JM Bae <jmbae@live.com>
2025-01-14 11:15:07 +0900

    챕터30: List

---
+++ 04-SwiftUI/LIstDemo/LIstDemo/LIstDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIstDemoApp.swift
+//  LIstDemo
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct LIstDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
