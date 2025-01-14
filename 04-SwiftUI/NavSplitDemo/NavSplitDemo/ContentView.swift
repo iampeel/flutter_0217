2301653e2d953cf88458b128040223fd5e95d96e
JM Bae <jmbae@live.com>
2025-01-14 15:12:36 +0900

    NavigationSplitView 추가, 데이터 추가

--- 04-SwiftUI/NavSplitDemo/NavSplitDemo/ContentView.swift
+++ 04-SwiftUI/NavSplitDemo/NavSplitDemo/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var categories = [
+        IconCategory(categoryName: "Folders", images: ["questionmark.folder.ar",
+                                                       "questionmark.folder",
+                                                       "questionmark.folder.fill.ar",
+                                                       "folder.fill.badge.gear",
+                                                       "questionmark.folder.fill"]),
+        IconCategory(categoryName: "Circles", images: ["book.circle",
+                                                       "books.vertical.circle",
+                                                       "books.vertical.circle.fill",
+                                                       "book.circle.fill",
+                                                       "book.closed.circle"]),
+        IconCategory(categoryName: "Clouds", images: ["cloud.rain",
+                                                      "cloud",
+                                                      "cloud.drizzle.fill",
+                                                      "cloud.fill",
+                                                      "cloud.drizzle"])
+    ]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView {
+
+        } content: {
+            
+        } detail: {
+            
         }
-        .padding()
     }
 }
 
