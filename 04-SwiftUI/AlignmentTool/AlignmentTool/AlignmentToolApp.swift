98d2e0b527ea8e6326dd52b1e3349ae756deefb5
JM Bae <jmbae@live.com>
2025-01-14 10:12:05 +0900

    AlignmentTool 프로젝트 추가

---
+++ 04-SwiftUI/AlignmentTool/AlignmentTool/AlignmentToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentToolApp.swift
+//  AlignmentTool
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 04-SwiftUI/AlignmentTool/AlignmentTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlignmentTool
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
