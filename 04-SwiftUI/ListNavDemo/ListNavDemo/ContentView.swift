01ea952110d3d466fc3eff564baf881b7de59319
JM Bae <jmbae@live.com>
2025-01-14 14:46:56 +0900

    네비게이션 경로 추가

--- 04-SwiftUI/ListNavDemo/ListNavDemo/ContentView.swift
+++ 04-SwiftUI/ListNavDemo/ListNavDemo/ContentView.swift
@@ -9,14 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State var carStore = CarStore(cars: carData)
+    @State var stackPath = NavigationPath()
     
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $stackPath) {
             List {
                 ForEach(carStore.cars) { car in
                     NavigationLink(value: car) {
                         ListCell(car: car)
                     }
+                }
+            }
+            .navigationTitle(Text("EV Cars"))
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading, content: {
+                    NavigationLink(value: "Add Car") {
+                        Text("Add")
+                            .foregroundStyle(.blue)
+                    }
+                })
+                ToolbarItem(placement: .topBarTrailing, content: {
+                    EditButton()
+                })
+            }
+            .navigationDestination(for: String.self) { menuString in
+                if menuString == "Add Car" {
+                    AddNewCar(carStore: carStore, path: $stackPath)
                 }
             }
             .navigationDestination(for: Car.self) { car in
