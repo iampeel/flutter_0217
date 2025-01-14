cee82295fd314b81b92ec2aee1f1b9518c171a00
JM Bae <jmbae@live.com>
2025-01-14 13:29:08 +0900

    NavigationStack, NavigationLink, navigationDestination(for:)

--- 04-SwiftUI/LIstDemo/LIstDemo/ContentView.swift
+++ 04-SwiftUI/LIstDemo/LIstDemo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 // Identifiable 프로토콜을 준수하는 ToDoItem 구조체 정의
 // UUID를 통해 각 항목을 고유하게 식별
-struct ToDoItem: Identifiable {
+struct ToDoItem: Identifiable, Hashable {
     let id: UUID = UUID()  // 자동으로 생성되는 고유 식별자
     var task: String       // 할 일 텍스트
     var imageName: String  // SF Symbols 이미지 이름
@@ -27,34 +27,55 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        List {
-            // 설정 섹션
-            Section(header: Text("Settings")) {
-                // 알림 허용 토글 스위치
-                Toggle(isOn: $toggleStatus) {
-                    Text("Allow Notifications")
+        NavigationStack {
+            List {
+                // 설정 섹션
+                Section(header: Text("Settings")) {
+                    // 알림 허용 토글 스위치
+                    Toggle(isOn: $toggleStatus) {
+                        Text("Allow Notifications")
+                    }
+                    
+                    NavigationLink(value: listData.count) {
+                        Text("View Task Count")
+                    }
                 }
-            }
-            
-            // ToDo 태스크 섹션
-            Section(header: Text("To Do Tasks")) {
-                // listData 배열의 각 항목을 순회하며 표시
-                ForEach(listData) { item in
-                    HStack {
-                        Image(systemName: item.imageName)  // SF Symbols 이미지
-                        Text(item.task)                    // 할 일 텍스트
+                
+                // ToDo 태스크 섹션
+                Section(header: Text("To Do Tasks")) {
+                    // listData 배열의 각 항목을 순회하며 표시
+                    ForEach(listData) { item in
+                        NavigationLink(value: item) {
+                            HStack {
+                                Image(systemName: item.imageName)  // SF Symbols 이미지
+                                Text(item.task)                    // 할 일 텍스트
+                            }
+                        }
                     }
                 }
             }
-        }
-        // 당겨서 새로고침 기능 추가
-        .refreshable {
-            // 새로운 ToDo 항목들로 리스트 갱신
-            listData = [
-                ToDoItem(task: "Order dinner", imageName: "dollarsign.circle.fill"),
-                ToDoItem(task: "Call financial advisor", imageName: "phone.fill"),
-                ToDoItem(task: "Sell the kids", imageName: "person.2.fill")
-            ]
+            // NavigationLink value 타입에 따른 뷰 빌더를 실행시켜주는 수정자
+            .navigationDestination(for: ToDoItem.self) { item in
+                // GET ToDoItem
+                Text("Number of tasks: \(item.task) ( \(item.id)")
+            }
+            .navigationDestination(for: Int.self) { count in
+                Text("Number of tasks: \(count)")
+            }
+            .navigationDestination(for: String.self) { task in
+                VStack {
+                    Text("Selected Task: \(task)")
+                }
+            }
+            // 당겨서 새로고침 기능 추가
+            .refreshable {
+                // 새로운 ToDo 항목들로 리스트 갱신
+                listData = [
+                    ToDoItem(task: "Order dinner", imageName: "dollarsign.circle.fill"),
+                    ToDoItem(task: "Call financial advisor", imageName: "phone.fill"),
+                    ToDoItem(task: "Sell the kids", imageName: "person.2.fill")
+                ]
+            }
         }
     }
 }
