48dce7304ade0098862027202d35070061f52909
JM Bae <jmbae@live.com>
2025-01-14 14:50:10 +0900

    편집, 삭제 버튼 추가

--- 04-SwiftUI/ListNavDemo/ListNavDemo/ContentView.swift
+++ 04-SwiftUI/ListNavDemo/ListNavDemo/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
                         ListCell(car: car)
                     }
                 }
+                .onDelete(perform: deleteItems)
+                .onMove(perform: moveItems)
             }
             .navigationTitle(Text("EV Cars"))
             .toolbar {
@@ -41,6 +43,14 @@ struct ContentView: View {
                 CarDetail(selectedCar: car)
             }
         }
+    }
+    
+    func deleteItems(offsets: IndexSet) {
+        carStore.cars.remove(atOffsets: offsets)
+    }
+    
+    func moveItems(from source: IndexSet, to destination: Int) {
+        carStore.cars.move(fromOffsets: source, toOffset: destination)
     }
 }
 
