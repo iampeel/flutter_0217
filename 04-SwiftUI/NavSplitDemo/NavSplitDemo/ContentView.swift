19b12664ba314f4cf22fb5d945f1ff3bdb8aa3ca
JM Bae <jmbae@live.com>
2025-01-14 15:22:45 +0900

    분할 네비게이션 환경 구성

--- 04-SwiftUI/NavSplitDemo/NavSplitDemo/ContentView.swift
+++ 04-SwiftUI/NavSplitDemo/NavSplitDemo/ContentView.swift
@@ -27,18 +27,38 @@ struct ContentView: View {
     ]
     
     @State private var selectedCategory: IconCategory?
-    
+    @State private var selectedImage: String?
+    @State private var columnVisibility = NavigationSplitViewVisibility.all
+
     var body: some View {
-        NavigationSplitView {
+        NavigationSplitView(columnVisibility: $columnVisibility) {
             List(categories, selection: $selectedCategory) { category in
                 Text(category.categoryName).tag(category)
             }
         } content: {
-            
+            if let selectedCategory {
+                List(selectedCategory.images, id: \.self, selection: $selectedImage) { image in
+                    HStack {
+                        Image(systemName: image)
+                        Text(image)
+                    }.tag(image)
+                }
+            } else {
+                Text("Select a category")
+            }
         } detail: {
-            
+            if let selectedImage {
+                Image(systemName: selectedImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+            } else {
+                Text("Select an image")
+            }
         }
+        .navigationSplitViewStyle(.balanced)
     }
+
 }
 
 #Preview {
