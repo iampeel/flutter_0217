656bb8e39d9dd963bfdce11be853965e8fa23f56
JM Bae <jmbae@live.com>
2025-01-14 14:04:15 +0900

    챕터 31

---
+++ 04-SwiftUI/ListNavDemo/ListNavDemo/ListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavDemoApp.swift
+//  ListNavDemo
+//
+//  Created by Jungman Bae on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
