597c5193a11a6a4a537f56b49adaac44758b9b27
JM Bae <jmbae@live.com>
2025-01-15 13:20:13 +0900

    챕터 39: 콘텍스트 메뉴 바인딩하기

---
+++ 04-SwiftUI/ContextMenuDemo/ContextMenuDemo/ContextMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuDemoApp.swift
+//  ContextMenuDemo
+//
+//  Created by Jungman Bae on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
