a30b8ce838045c0da3f0a0c1633fbedaa88ee4ae
JM Bae <jmbae@live.com>
2025-01-15 15:38:52 +0900

    다양한 차트 그리기

--- 04-SwiftUI/44-ChartDemo/ChartDemo/ContentView.swift
+++ 04-SwiftUI/44-ChartDemo/ChartDemo/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import Charts
+
+struct MonthlyTemp: Identifiable {
+    var id = UUID()
+    var month: String
+    var degrees: Int
+}
+
+let tempData: [MonthlyTemp] = [
+    MonthlyTemp(month: "Jan", degrees: 50),
+    MonthlyTemp(month: "Feb", degrees: 43),
+    MonthlyTemp(month: "Mar", degrees: 61)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart(tempData) { data in
+            AreaMark(
+                x: .value("Month", data.month),
+                y: .value("Temp", data.degrees)
+            )
+//            RectangleMark(
+//                x: .value("Month", data.month),
+//                y: .value("Temp", data.degrees)
+//            )
+//            LineMark(
+//                x: .value("Month", data.month),
+//                y: .value("Temp", data.degrees)
+//            )
         }
-        .padding()
     }
 }
 
