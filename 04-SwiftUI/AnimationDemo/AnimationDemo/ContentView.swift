dcdcd1b6fd101bb7b4e5192599eba056384dac44
JM Bae <jmbae@live.com>
2025-01-15 14:10:27 +0900

    각종 트랜지션 실습

--- 04-SwiftUI/AnimationDemo/AnimationDemo/ContentView.swift
+++ 04-SwiftUI/AnimationDemo/AnimationDemo/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTextVisible: Bool = false
     
     var body: some View {
-        Text("")
+        VStack(spacing: 20) {
+            Button(action: {
+                withAnimation(.easeInOut(duration: 1)) {
+                    isTextVisible.toggle()
+                }
+            }, label: {
+                Text("Toggle Text")
+                    .font(.title2)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(Color.blue)
+                    .clipShape(.rect(cornerRadius: 10))
+            })
+            
+            if isTextVisible {
+                Text("Hello SwiftUI Animation!")
+                    .font(.largeTitle)
+                    .padding()
+                    .transition(.opacity)
+//                    .transition(.scale)
+//                    .transition(.slide)
+//                    .transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
