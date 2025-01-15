904b0883a8e70b4452ffea88cbbff45231e50a31
JM Bae <jmbae@live.com>
2025-01-15 13:52:10 +0900

    챕터 41: 애니메이션

---
+++ 04-SwiftUI/AnimationDemo/AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by Jungman Bae on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
