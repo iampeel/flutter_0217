3793028198e8ac3055568c9dc53bf32de353263e
JM Bae <jmbae@live.com>
2025-01-15 13:26:55 +0900

    컨텍스트 메뉴

--- 04-SwiftUI/ContextMenuDemo/ContextMenuDemo/ContentView.swift
+++ 04-SwiftUI/ContextMenuDemo/ContextMenuDemo/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var foregroundColor: Color = .yellow
+    @State private var backgroundColor: Color = .black
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, World!")
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(foregroundColor)
+            .background(backgroundColor)
+//        앱을 실행하고 "Hello, world!" 텍스트를 길게 누르면 컨텍스트 메뉴가 나타납니다. "Inverted Colors" 옵션을 선택하면 텍스트의 색상이 반전됩니다.
+            .contextMenu {
+                Button(action: {
+                    self.foregroundColor = .black
+                    self.backgroundColor = .white
+                }, label: {
+                    Text("Normal Colors")
+                    Image(systemName: "paintbrush")
+                })
+                Button(action: {
+                    self.foregroundColor = .white
+                    self.backgroundColor = .black
+                }, label: {
+                    Text("Inverted Colors")
+                    Image(systemName: "paintbrush.fill")
+                })
+            }
+        
     }
 }
 
