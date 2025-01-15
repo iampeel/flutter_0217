5d11271d3801c4d4d7c47cfe8ec84bf8274a53bb
JM Bae <jmbae@live.com>
2025-01-15 13:08:21 +0900

    TabView, tabItem, tag, selection

--- 04-SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
+++ 04-SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection: Int = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selection) {
+            Text("First Content View")
+                .tabItem {
+                    Image(systemName: "1.circle")
+                    Text("Screen One")
+                }
+                .tag(0)
+            Text("Second Content View")
+                .tabItem {
+                    Image(systemName: "2.circle")
+                    Text("Screen Two")
+                }
+                .tag(1)
+            Text("Third Content View")
+                .tabItem {
+                    Image(systemName: "3.circle")
+                    Text("Screen Three")
+                }
+                .tag(2)
         }
-        .padding()
+        .font(.largeTitle)
+//        .tabViewStyle(PageTabViewStyle())
     }
 }
 
