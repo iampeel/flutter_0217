5c84563205688d8ca7ee2b453ca9d4cbdf0f12a8
JM Bae <jungman82@gmail.com>
2025-01-16 09:24:02 +0900

    챕터 46: DocumentGroup

---
+++ 04-SwiftUI/46-ImageDocDemo/ImageDocDemo/ImageDocDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDocDemoApp.swift
+//  ImageDocDemo
+//
+//  Created by Jungman Bae on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageDocDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
