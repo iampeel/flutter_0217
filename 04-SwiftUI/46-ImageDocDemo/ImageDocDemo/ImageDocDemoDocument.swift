b0645ac38d3c06a48b0398dfb65ad2105b48070b
JM Bae <jungman82@gmail.com>
2025-01-16 09:50:26 +0900

    Document 타입 변경

--- 04-SwiftUI/46-ImageDocDemo/ImageDocDemo/ImageDocDemoDocument.swift
+++ 04-SwiftUI/46-ImageDocDemo/ImageDocDemo/ImageDocDemoDocument.swift
@@ -9,31 +9,33 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 extension UTType {
-    static var exampleText: UTType {
-        UTType(importedAs: "com.example.plain-text")
+    static var exampleImage: UTType {
+        UTType(importedAs: "com.ebookfrenzy.image")
     }
 }
 
 struct ImageDocDemoDocument: FileDocument {
-    var text: String
-
-    init(text: String = "Hello, world!") {
-        self.text = text
+    var image: UIImage = UIImage()
+    
+    init() {
+        if let image = UIImage(named: "cascaedfalls") {
+            self.image = image
+        }
     }
 
-    static var readableContentTypes: [UTType] { [.exampleText] }
+    static var readableContentTypes: [UTType] { [.exampleImage] }
 
     init(configuration: ReadConfiguration) throws {
         guard let data = configuration.file.regularFileContents,
-              let string = String(data: data, encoding: .utf8)
+              let decodedImage: UIImage = UIImage(data: data)
         else {
             throw CocoaError(.fileReadCorruptFile)
         }
-        text = string
+        image = decodedImage
     }
     
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = text.data(using: .utf8)!
+        let data = image.pngData()!
         return .init(regularFileWithContents: data)
     }
 }
