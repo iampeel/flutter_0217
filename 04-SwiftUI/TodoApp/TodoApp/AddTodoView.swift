3cce533d7d604d6023e12f6ffe4730a33071d49c
JM Bae <jungman82@gmail.com>
2025-01-20 11:23:34 +0900

    TodoItem 새로생성 페이지(New Todo) 추가

---
+++ 04-SwiftUI/TodoApp/TodoApp/AddTodoView.swift
@@ -0,0 +1,46 @@
+//
+//  AddTodoView.swift
+//  TodoApp
+//
+//  Created by Jungman Bae on 1/20/25.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    // 데이터 저장소에 접근할 수 있는 환경 변수
+    @Environment(\.modelContext) private var modelContext
+    // 나를 호출한 뷰에서 닫기 기능을 동작 시키는 환경 변수(클로저)
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var title: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Title", text: $title)
+                }
+            }
+            .navigationTitle("New Todo")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        let todo = TodoItem(title: title)
+                        modelContext.insert(todo)
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddTodoView()
+}
