6709b8486675c280d07cb0df6dcec7d787aa9c75
JM Bae <jungman82@gmail.com>
2025-01-20 09:31:21 +0900

    챕터 53: 시리킷

---
+++ 04-SwiftUI/53-SiriDemo/SiriDemo/SiriDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriDemoApp.swift
+//  SiriDemo
+//
+//  Created by Jungman Bae on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SiriDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
