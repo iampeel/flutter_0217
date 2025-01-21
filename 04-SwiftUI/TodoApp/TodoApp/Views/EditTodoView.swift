430b40970884ce73ed7bc79917cddf35e431e296
JM Bae <jungman82@gmail.com>
2025-01-21 13:50:40 +0900

    마감일 데이터 추가, 수정, 투두 아이템 마감일 표시

--- 04-SwiftUI/TodoApp/TodoApp/Views/EditTodoView.swift
+++ 04-SwiftUI/TodoApp/TodoApp/Views/EditTodoView.swift
@@ -14,11 +14,15 @@ struct EditTodoView: View {
     
     @State private var title: String = ""
     @State private var priority: Priority
+    @State private var dueDateEnabled = false
+    @State private var dueDate: Date? = nil
     
     init(todo: TodoItem) {
         self.todo = todo
         self._title = State(initialValue: todo.title)
         self._priority = State(initialValue: todo.priority)
+        self._dueDateEnabled = State(initialValue: todo.dueDate != nil)
+        self._dueDate = State(initialValue: todo.dueDate)
     }
     
     var body: some View {
@@ -34,6 +38,16 @@ struct EditTodoView: View {
                             .tag(priority)
                     }
                 }
+                Toggle("마감일 설정", isOn: $dueDateEnabled)
+                if dueDateEnabled {
+                    DatePicker("마감일",
+                               selection: Binding(get: {
+                        dueDate ?? Date()
+                    }, set:{
+                        dueDate = $0
+                    }))
+                }
+
             }
         }
         .navigationTitle("Edit Todo")
@@ -43,6 +57,7 @@ struct EditTodoView: View {
                     // 수정 기능
                     todo.title = title
                     todo.priority = priority
+                    todo.dueDate = dueDateEnabled ? dueDate : nil
                     // 뷰 닫기와 동시에 모델 컨텍스트 저장이 호출된다.
                     dismiss()
                 }
