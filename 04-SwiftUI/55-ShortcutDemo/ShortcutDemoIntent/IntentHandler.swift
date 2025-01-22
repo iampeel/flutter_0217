34e071fb59a60cee2aa01c1bd5291dbf62ddd242
JM Bae <jungman82@gmail.com>
2025-01-22 10:12:31 +0900

    시리킷 인텐트 정의 파일 추가, 앱 그룹 지정, 인텐트 설정, 파라미터 설정

---
+++ 04-SwiftUI/55-ShortcutDemo/ShortcutDemoIntent/IntentHandler.swift
@@ -0,0 +1,19 @@
+//
+//  IntentHandler.swift
+//  ShortcutDemoIntent
+//
+//  Created by Jungman Bae on 1/22/25.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
