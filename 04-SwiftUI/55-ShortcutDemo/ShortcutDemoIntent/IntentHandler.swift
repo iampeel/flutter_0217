87e26046e76a7d4cc623712564c09f4ccce32cea
JM Bae <jungman82@gmail.com>
2025-01-22 10:49:28 +0900

    핸들러 코드 수정 #1

--- 04-SwiftUI/55-ShortcutDemo/ShortcutDemoIntent/IntentHandler.swift
+++ 04-SwiftUI/55-ShortcutDemo/ShortcutDemoIntent/IntentHandler.swift
@@ -7,13 +7,35 @@
 
 import Intents
 
-class IntentHandler: INExtension {
-    
+class IntentHandler: INExtension, BuyStockIntentHandling {
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
+        guard intent is BuyStockIntent else {
+            fatalError("Unkown intent type: \(intent)")
+        }
         
         return self
     }
     
+    func handle(intent: BuyStockIntent, completion: @escaping (BuyStockIntentResponse) -> Void) {
+        
+    }
+    
+    func resolveSymbol(for intent: BuyStockIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        if let symbol = intent.symbol {
+            completion(INStringResolutionResult.success(with: symbol))
+        } else {
+            completion(INStringResolutionResult.needsValue())
+        }
+    }
+        
+    func resolveQuantity(for intent: BuyStockIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        if let quantity = intent.quantity {
+            completion(INStringResolutionResult.success(with: quantity))
+        } else {
+            completion(INStringResolutionResult.needsValue())
+        }
+    }
+
 }
