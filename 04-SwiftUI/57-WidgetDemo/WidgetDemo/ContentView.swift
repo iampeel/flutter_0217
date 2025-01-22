76c8282f867ce437b4ba980f1d30690af875479c
JM Bae <jungman82@gmail.com>
2025-01-22 13:23:09 +0900

    챕터 57: 위젯킷

---
+++ 04-SwiftUI/57-WidgetDemo/WidgetDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetDemo
+//
+//  Created by Jungman Bae on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
