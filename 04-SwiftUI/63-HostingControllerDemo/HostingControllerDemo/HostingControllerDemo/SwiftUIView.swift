2a849f559030ac89b28850806a96a06ad610c5aa
JM Bae <jungman82@gmail.com>
2025-01-23 15:34:02 +0900

    호스팅 컨틀롤러 추가까지

---
+++ 04-SwiftUI/63-HostingControllerDemo/HostingControllerDemo/HostingControllerDemo/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  HostingControllerDemo
+//
+//  Created by Jungman Bae on 1/23/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
