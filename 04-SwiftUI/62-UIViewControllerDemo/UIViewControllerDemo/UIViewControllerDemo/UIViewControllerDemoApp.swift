9094d445be2101657f03ffda8e98cf6ab46a3fe5
JM Bae <jungman82@gmail.com>
2025-01-23 14:11:28 +0900

    챕터 62: 뷰 컨트롤러

---
+++ 04-SwiftUI/62-UIViewControllerDemo/UIViewControllerDemo/UIViewControllerDemo/UIViewControllerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerDemoApp.swift
+//  UIViewControllerDemo
+//
+//  Created by Jungman Bae on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
