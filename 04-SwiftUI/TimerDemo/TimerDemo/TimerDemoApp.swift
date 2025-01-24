444d511126d6a36f7a23b3be086d4066861d29bb
JM Bae <jungman82@gmail.com>
2025-01-24 09:15:59 +0900

    타이머 데모 프로젝트 시작

---
+++ 04-SwiftUI/TimerDemo/TimerDemo/TimerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerDemoApp.swift
+//  TimerDemo
+//
+//  Created by Jungman Bae on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
