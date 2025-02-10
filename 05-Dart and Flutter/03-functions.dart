efffc550858c048f743151463a930ea2ab7d54db
JM Bae <jungman82@gmail.com>
2025-02-10 14:16:30 +0900

    함수 반환 값 추가

--- 05-Dart and Flutter/03-functions.dart
+++ 05-Dart and Flutter/03-functions.dart
@@ -8,11 +8,28 @@ void main() {
   var hourDifference = -7;
   getCurrentDateTimeWithDifference(hourDifference);
 
+  // 파라미터 생략 가능한 함수 호출
   printGreetingNamed();
   printGreetingNamed(personName: "Rich");
   printGreetingNamed(personName: "Mary", clientId: 001);
+
+  // 실습: 반환 값이 있는 함수
+  DateTime timeNow = getTimeDifference(0);
+  DateTime timeDifference = getTimeDifference(-7);
+
+  print('Current time: $timeNow');
+  print('Time with Difference: $timeDifference');
 }
 
+// 함수 선언: 반환 값이 있는 함수
+DateTime getTimeDifference(int hourDifference) {
+  DateTime timeNow = DateTime.now();
+  DateTime timeDifference = timeNow.add(Duration(hours: hourDifference));
+
+  return timeDifference;
+}
+
+// 이름이 있는 파라미터 함수 선언, 기본값 설정
 void printGreetingNamed({String personName = 'Stranger', int clientId = 999}) {
   if (personName.contains('Stranger')) {
     print('Employee: $clientId, Stranger danger!');
