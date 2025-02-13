ff5c957da474bf105b9164bea93dcb112298a9e2
JM Bae <jungman82@gmail.com>
2025-02-13 15:55:53 +0900

    텍스트 필드 데모 및 AlertDialog 관련 주석 추가

--- 05-Dart_and_Flutter/list_navigation/lib/text_field_demo.dart
+++ 05-Dart_and_Flutter/list_navigation/lib/text_field_demo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// TextFieldDemo는 텍스트 필드 예제를 보여주는 StatelessWidget입니다.
 class TextFieldDemo extends StatelessWidget {
   const TextFieldDemo({super.key});
 
@@ -9,6 +10,7 @@ class TextFieldDemo extends StatelessWidget {
   }
 }
 
+// TextFieldExample은 텍스트 필드와 값을 표시하는 AlertDialog를 포함하는 StatefulWidget입니다.
 class TextFieldExample extends StatefulWidget {
   const TextFieldExample({super.key});
 
@@ -16,12 +18,16 @@ class TextFieldExample extends StatefulWidget {
   State<TextFieldExample> createState() => _TextFieldExampleState();
 }
 
+// _TextFieldExampleState는 TextFieldExample의 상태를 관리합니다.
 class _TextFieldExampleState extends State<TextFieldExample> {
+  // _controller는 텍스트 필드의 값을 제어합니다.
   final TextEditingController _controller = TextEditingController();
+  // _passwordController는 비밀번호 텍스트 필드의 값을 제어합니다.
   final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold는 기본적인 앱 화면 구조를 제공합니다.
     return Scaffold(
       appBar: AppBar(
         title: const Text('Text Field Example'),
@@ -30,6 +36,7 @@ class _TextFieldExampleState extends State<TextFieldExample> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
+            // TextField는 사용자로부터 텍스트 입력을 받는 위젯입니다.
             TextField(
               controller: _controller,
               decoration: const InputDecoration(
@@ -44,8 +51,10 @@ class _TextFieldExampleState extends State<TextFieldExample> {
               // obscureText 속성을 사용하여 입력한 텍스트를 숨깁니다.
               obscureText: true,
             ),
+            // ElevatedButton은 클릭 시 AlertDialog를 표시하는 버튼입니다.
             ElevatedButton(
               onPressed: () {
+                // showDialog는 AlertDialog를 표시하는 함수입니다.
                 showDialog(
                   context: context,
                   builder: (context) {
