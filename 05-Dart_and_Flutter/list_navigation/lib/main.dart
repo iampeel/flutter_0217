db1f500b43291cc8432910f2205026feec6807ca
JM Bae <jungman82@gmail.com>
2025-02-17 09:30:44 +0900

    내비게이터 데모 위젯 추가 및 pubspec.lock 의존성 업데이트

--- 05-Dart_and_Flutter/list_navigation/lib/main.dart
+++ 05-Dart_and_Flutter/list_navigation/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'shared_preference_demo.dart';
+import 'navigator_demo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: SharedPreferenceDemo(),
+      home: NavigatorDemo(),
     );
   }
 }
